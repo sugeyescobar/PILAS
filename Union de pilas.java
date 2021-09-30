c6de241f226c7ba51a09d67adfdbcbd40e478ccf
sugeyescobar <90210794+sugeyescobar@users.noreply.github.com>
2021-09-30 08:01:42 -0500

    Add files via upload

--- Union de pilas.java
+++ Union de pilas.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class PILAS {
     public static void main(String[] args) {
         Scanner PILAS = new Scanner(System.in);
-        int cont=0,    opcion=0,   LONG;
+        int CONT=0,    OP=0,   LONG;
         System.out.println("INGRESAR TAMAÑO DE LA PILA");
         LONG=PILAS.nextInt();
         int PILAa[] = new int[LONG];
@@ -22,21 +22,21 @@ public class PILAS {
                     + "4. UNIRFICACION DE PILAS B\n"
                     + "5. Mostrar pila \n"
                     + "6. Salida\n");
-            switch (opcion = PILAS.nextInt()) {
+            switch (OP = PILAS.nextInt()) {
                 case 1:
-                    if(cont == PILAa.length){
+                    if(CONT == PILAa.length){
                         System.out.println("LA PILA ESTA LLENA....");
                     }else{
-                        for(int i=cont;i<PILAa.length;i++){
-                            PILAa[cont]=(int)(Math.random()*(100-1)+(1));
-                            PILAb[cont]=(int)(Math.random()*(200-100)+(100));
-                            cont++;
+                        for(int i=CONT;i<PILAa.length;i++){
+                            PILAa[CONT]=(int)(Math.random()*(100-1)+(1));
+                            PILAb[CONT]=(int)(Math.random()*(200-100)+(100));
+                            CONT++;
                         }
                     }
                 break;
                 case 2:
-                    if(cont >0){
-                        for (int i= cont-1; i>=0;i--){
+                    if(CONT >0){
+                        for (int i= CONT-1; i>=0;i--){
                             System.out.println(PILAa[i] + " " + PILAb[i]);
                         }
                     }else{
@@ -44,10 +44,10 @@ public class PILAS {
                     }
                 break;
                 case 3:
-                    if(cont > 0){ 
+                    if(CONT > 0){ 
                             int i=0;
                                 while(i<=LONG-1){
-                                    cont--;
+                                    CONT--;
                                     i++;
                                 }
                     }else{
@@ -55,8 +55,8 @@ public class PILAS {
                     }
                 break;
                 case 4:
-                    if(cont > 0){
-                        int j=0,i=cont-1;
+                    if(CONT > 0){
+                        int j=0,i=CONT-1;
                         while(i>=0){
                             PILAc[j]=PILAa[i]+PILAb[i];
                             j++;
@@ -67,8 +67,8 @@ public class PILAS {
                     }
                 break;
                 case 5:
-                if(cont >0){
-                    int i=cont-1;
+                if(CONT >0){
+                    int i=CONT-1;
                     while(i>=0){
                         System.out.println(PILAc[i]);
                         i--;
@@ -79,9 +79,7 @@ public class PILAS {
                 break;
                 
          }
-        }while(opcion != 6);       
+        }while(OP != 6);       
     }
 }
-    
-
     
